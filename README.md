0f8c79de47e4d33e7b8420d1767b9dd7db7d3617
Hugo Aquino <hugo.aquino.navarrete@gmail.com>
2020-10-20 01:06:27 -0500

    Agregando README.md y main.tf

---
+++ README.md
@@ -0,0 +1,61 @@
+# Creación de "n" instancias EC2 tipo ubuntu en AWS usando Terraform
+
+Script en Terraform que automatiza el despliegue en AWS n instancias EC2 tipo ubuntu con acceso a internet que permiten tráfico SSH, HTTP y HTTPS
+
+## 1. Configura AWS (este script corre en la región "us-west-2")
+Antes de ejecutar este script, ejecuta `aws configure` para habilitar
+   - AWS Access Key ID
+   - AWS Secret Access Key
+   - Default region name 
+   - Default output format (json,yaml,yaml-stream,text,table)
+
+## 2. Genera un par de llaves rsa pública/privada
+   ```bash 
+   ssh-keygen
+   ```
+   Sálvala en el directorio donde correras este script `<ruta_absoluta>/linux-training-key`, deja vacío `passphrase`
+
+## 3. Conexión por SSH a la máquina virtual 
+   ```bash
+   ssh -v -l ubuntu -i linux-training-key <ip_publica_instancia_ec2>
+   ```
+## 4. Script compatible con la versión de Terraform v0.11.3, estos son los pasos para descargarlo e instalarlo
+   ```bash
+  wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
+  unzip terraform_0.13.3_linux_amd64.zip
+  sudo mv terraform /usr/local/bin/
+  terraform --version 
+   ```
+
+## 5. Para ejecutar el script `terraform apply -var "nombre_instancia=<nombre_recursos>" -var "cantidad_instancias=<n>"` cuando el siguiente mensaje aparezca, escribe `yes`:
+   ```bash
+   Do you want to perform these actions?
+     Terraform will perform the actions described above.
+     Only 'yes' will be accepted to approve.
+
+     Enter a value:
+   ```
+
+Una vez el script se ejecuta generará un mensaje parecido a esto:
+
+   ```bash
+   Apply complete! Resources: <cantidad_recursos> added, 0 changed, 0 destroyed.
+   ```
+
+## 6. Para eliminar la infraestructura desplegada, ejecuta `terraform destroy` y cuando aparezca el siguiente mensaje, escribe `yes`:
+   ```bash
+   Do you really want to destroy?
+     Terraform will destroy all your managed infrastructure, as shown above.
+     There is no undo. Only 'yes' will be accepted to confirm.
+
+     Enter a value:
+   ```
+
+El script una vez ejecutado generará un mensaje parecido a esto:
+
+   ```bash
+   Destroy complete! Resources: <cantidad_recursos> destroyed.
+   ```
+
+## 7. Valida en el portal de AWS que los recursos se hayan eliminado
+Las instancias EC2 deberan aparecen con estado `Terminated` y después de algunos minutos desaparecerán de la consola
---
+++ main.tf
@@ -0,0 +1,165 @@
+# Script en Terraform para desplegar en AWS n instancias EC2 tipo ubuntu 
+# con acceso a internet que permiten tráfico SSH, HTTP y HTTPS
+# Hugo Aquino
+# Octubre 2020
+
+# Antes de ejecutar este script, ejecuta "aws configure" para poder habilitar
+# AWS Access Key ID
+# AWS Secret Access Key
+# Default region name
+# Default output format (YAML)
+
+# Después genera una llave ejecutando
+# "ssh-keygen"
+# Sálvalo en el directorio donde este este script <ruta_completa>/linux-training-key
+# Deja en blanco "passphrase"
+
+# Para conectarte con la VM una vez creada
+# ssh -v -l ubuntu -i linux-training-key <ip_publica_instancia_creada> 
+
+# Para correr este script desde la consola:
+# terraform apply -var "nombre_instancia=<nombre_recursos>" -var "cantidad_instancias=<n>"
+
+# Para ajustar la cantidad de VMs a crear hay que cambiar el valor de la siguiente variable a la cantidad "default = n"
+
+# Variable para saber cuantas instancias crear
+variable cantidad_instancias {
+  default = 1
+}
+
+# Para ajustar el nombre de los recursos hay que cambiar el valor de la siguiente variable al nombre que desees "default = <nombre>"
+
+variable nombre_instancia {
+  default = "prueba"
+}
+
+# Haremos despliegue en AWS
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+# Generemos una llave
+resource "aws_key_pair" "key" {
+  key_name   = "key"
+  public_key = "${file("key.pub")}"
+}
+
+# Crea la VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.1.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+ 
+  tags = {
+    Name = "${var.nombre_instancia}-vpc"
+  }
+}
+
+# Crea un gateway de Internet
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+ 
+  tags = {
+    Name = "${var.nombre_instancia}-internet-gateway"
+  }
+}
+
+# Crea las subredes internas
+resource "aws_subnet" "subnet" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "${var.nombre_instancia}-subnet"
+  }
+}
+
+# Tabla de ruteo para internet
+resource "aws_route_table" "route-table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.internet-gateway.id}"
+  }
+
+  tags {
+    Name = "${var.nombre_instancia}-tabla-ruteo-acceso-internet"
+  }
+}
+
+# Asocia la tabla de ruteo a la subred
+resource "aws_route_table_association" "subnet-asociacion" {
+  subnet_id      = "${aws_subnet.subnet.id}"
+  route_table_id = "${aws_route_table.route-table.id}"
+}
+
+# Crea el grupo de seguridad
+resource "aws_security_group" "security-group" {
+  name        = "${var.nombre_instancia}-security-group"
+  description = "Permite trafico entrante"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  # Permite trafico SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  # Permite trafico HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Permite trafico HHTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Permite acceso a Internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.nombre_instancia}-security-group"
+  }
+}
+
+# Crea n instancias Ubuntu
+resource "aws_instance" "linux-training" {
+  count                       = "${var.cantidad_instancias}"
+  ami                         = "ami-0d1cd67c26f5fca19"
+  instance_type               = "t2.micro"
+  key_name                    = "${aws_key_pair.key.key_name}"
+  vpc_security_group_ids      = ["${aws_security_group.security-group.id}"]
+  subnet_id                   = "${aws_subnet.subnet.id}"
+  associate_public_ip_address = "true"
+
+  root_block_device {
+    volume_size           = "10"
+    volume_type           = "standard"
+    delete_on_termination = "true"
+  }
+
+  tags = {
+    Name = "${var.nombre_instancia}-${count.index + 1}"
+  }
+
+}
